07500d904716c7fd34a1773c25b6fb9a37b778aa
Kartik Sinoliya <kv8455@gmail.com>
2022-12-12 16:53:03 +0530

    learning python in a hard way excercises

---
+++ ex6.py
@@ -0,0 +1,22 @@
+# strings and text excercise.
+x = "There are %d types of people." %10
+binary = "binary"
+do_not = "don't"
+y = "Those who knows %s and those who %s." % (binary, do_not)
+
+print (x)
+print (y)
+
+print ("I said: %r" % x)
+print ("I also said: %r" % y)
+
+hilarious = False
+joke_evolution = ("Isn't that joke so funny?! %r")
+
+print (joke_evolution % hilarious)
+
+w = "This is the left side of ..."
+e = "a string with a right side."
+
+print (w + e)
+
